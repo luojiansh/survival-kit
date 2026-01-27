5ddb73a628a574c522e3acd5e6599041787ad5db
Jian Luo <jian.luo.cn@gmail.com>
2026-01-27 13:40:46 +0100

    move python to nixos

--- nixos/hosts/profiles/common/configuration.nix
+++ nixos/hosts/profiles/common/configuration.nix
@@ -16,6 +16,7 @@
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
     neovim
+    python313
   ];
 
   # Global Configuration
