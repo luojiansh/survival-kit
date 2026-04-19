0532b9fc1ee5dd114673b7834a758168be630180
Jian Luo <jian.luo.cn@gmail.com>
2026-04-19 15:15:03 +0200

    feat: merge darwin and lazyvim settings into console module

--- nixos/users/modules/console/home.nix
+++ nixos/users/modules/console/home.nix
@@ -143,6 +143,7 @@ fi
   # Neovim
   programs.neovim = {
     enable = true;
+    defaultEditor = true;
     vimAlias = true;
     viAlias = true;
     plugins = with pkgs; [
@@ -154,5 +155,36 @@ fi
       lua-language-server
     ];
   };
-  programs.lazyvim.enable = true;
+  programs.lazyvim = {
+    enable = true;
+
+    # Core LazyVim dependencies (git, ripgrep, fd, etc.)
+    installCoreDependencies = true; # default: true
+
+    extras = {
+      lang.nix.enable = true;
+      lang.python = {
+        enable = true;
+        installDependencies = true; # Install ruff
+        installRuntimeDependencies = true; # Install python3
+      };
+      lang.go = {
+        enable = true;
+        installDependencies = true; # Install gopls, gofumpt, etc.
+        installRuntimeDependencies = true; # Install go compiler
+      };
+    };
+
+    # Additional packages (optional)
+    extraPackages = with pkgs; [
+      nixd # Nix LSP
+      alejandra # Nix formatter
+    ];
+
+    # Only needed for languages not covered by LazyVim extras
+    treesitterParsers = with pkgs.vimPlugins.nvim-treesitter-parsers; [
+      wgsl # WebGPU Shading Language
+      templ # Go templ files
+    ];
+  };
 }
